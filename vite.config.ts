1310ddc361422bbb908209d19f2cc7ee151fb50a
zeck00 <a2005sdcardemom@gmail.com>
2025-01-15 14:32:10 +0400

    vite fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist',
   },
 });
